c1ab46ff70f3a0fe86f40142afb6562c9d838ad4
Nimrod Kor <nimrod@bridgecrew.io>
2021-06-22 10:42:49 +0300

    Update yor to 0.2.0-1f5813e

--- yor.rb
+++ yor.rb
@@ -1,7 +1,7 @@
 class Yor < Formula
   desc "Extensible auto-tagger for your IaC files"
   homepage "https://www.bridgecrew.io"
-  url "https://github.com/bridgecrewio/yor.git", tag: "0.2.0-4adcdc0", revision: "4adcdc00755d5aa00dd6b0cbbcb4d59d7bb51690"
+  url "https://github.com/bridgecrewio/yor.git", tag: "0.2.0-1f5813e", revision: "1f5813eea87ce6e0168d267c7beafd0565deb0e6"
   sha256 "5c3e44b89ced43365bb91405001fbb7eef5b48b0cea61ace68b6a44efbbb2b8e"
   license "Apache-2.0"
 
