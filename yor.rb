c76f28db92b0b82d551297472cf20f670d8f288e
Steve G <9045562+eurogig@users.noreply.github.com>
2021-07-28 14:27:51 +0100

    Updated to 0.1.85 (#2)
    Co-authored-by: Rotem Avni <52502521+rotemavni@users.noreply.github.com>

--- yor.rb
+++ yor.rb
@@ -2,8 +2,8 @@ class Yor < Formula
   desc "Extensible auto-tagger for your IaC files"
   homepage "https://www.bridgecrew.io"
   version "0.1.85"
-  url "https://github.com/bridgecrewio/yor.git", tag: "0.1.85", revision: "4b31cf47f2838eb0c737dcf88f62f626e2f661b1"
-  sha256 "214078db6f69eeade5552d21a35c66b66843c7849ccb3d91e15dcc1755a69e3b"
+  url "https://github.com/bridgecrewio/yor/releases/download/" + version + "/yor-" + version + "-darwin-amd64.tar.gz"
+  sha256 "2f6223b9196f335295afa920ea4a04093cf48b8970bd35d9cb15783e2a4919b9"
   license "Apache-2.0"
 
   def install
