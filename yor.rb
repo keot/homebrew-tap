9dcbbfabf681f2f4d9c96deeed003381b6ac792b
GitHub Action <action@github.com>
2021-08-19 18:38:54 +0300

    Brew formula update for yor version 0.1.95

--- yor.rb
+++ yor.rb
@@ -5,21 +5,21 @@
 class Yor < Formula
   desc "Extensible auto-tagger for your IaC files"
   homepage "https://www.yor.io"
-  version "0.1.94"
+  version "0.1.95"
   license "Apache-2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/bridgecrewio/yor/releases/download/0.1.94/yor_0.1.94_darwin_amd64.tar.gz"
-      sha256 "dc5c10a44dd213c5a58921ce423bb16285a6c864280e6574b4de5f50b4e3338e"
+      url "https://github.com/bridgecrewio/yor/releases/download/0.1.95/yor_0.1.95_darwin_amd64.tar.gz"
+      sha256 "4d448e49937df576341547ac382a18a8ebaf84c3550b6a9d3f640ec8982ffb6e"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/bridgecrewio/yor/releases/download/0.1.94/yor_0.1.94_linux_amd64.tar.gz"
-      sha256 "32fa223697a4e789b30656ef95f70487383dc34bee5cf1ff5a5c1b3f725d9ab1"
+      url "https://github.com/bridgecrewio/yor/releases/download/0.1.95/yor_0.1.95_linux_amd64.tar.gz"
+      sha256 "cc884b43ef03ae8cb55455828e5512d3a8f97aa01db7a2dda276d86bfbead195"
     end
   end
 
